d72fd56ab6b2c3bf054211b38cba6a60088497da
ayumu-1212 <abe.ayumu.infratop@gmail.com>
2019-08-06 16:06:03 +0900

    [Add]home,users,booksコントローラの配置 routingの設定 ヘッダーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'home#top'
+  get 'home/about', as: "about"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	resources :users, :books
 end
