fac546e23a19cc8103dfbab7a6a59706e194ec61
ayumu-1212 <abe.ayumu.infratop@gmail.com>
2019-08-07 16:19:48 +0900

    [Add]user/index/view・showを作成 refile系install

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,13 @@ class UsersController < ApplicationController
   end
 
   def show
+		@books = Book.where(user_id: params[:id])
   end
 
   def edit
   end
 
   def index
+		@users = User.all.order(name: "DESC")
   end
 end
