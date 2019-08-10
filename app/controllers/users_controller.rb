7d1826266a2383354be0530cf31a59674f4f1741
ayumu-1212 <abe.ayumu.infratop@gmail.com>
2019-08-11 02:10:45 +0900

    [Add]結構追加した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,30 @@
 class UsersController < ApplicationController
-  def new
-  end
 
   def show
-		@books = Book.where(user_id: params[:id])
+    @books = Book.where(user_id: params[:id])
+    @user = User.find(params[:id])
+    @newbook = Book.new
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    @user.save
+    redirect_to user_path(@user)
   end
 
   def index
-		@users = User.all.order(name: "DESC")
+    @users = User.all.order(name: "DESC")
+    @user = current_user
+    @newbook = Book.new
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction)
   end
 end
