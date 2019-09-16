8f0319d21a8dde4455ed87ec2a9448394bec4c88
ayumu-1212 <abe.ayumu.infratop@gmail.com>
2019-09-17 02:47:16 +0900

    :bookmark: いいね一覧をユーザー詳細に実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
 	has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :liked_books, through: :likes, source: :book
 
   has_many :relationships, class_name: "Relationship", foreign_key: "user_id"
   has_many :followings, through: :relationships, source: :follow
